625cf48e9ce7f2361041a9ab49cdc85d1188a7bb
mahendramahara <mahendramahara369@gmail.com>
2023-01-09 07:00:00 +0545

    Added solved ASP.NET project 🔥

---
+++ QN2/QN2/QN2.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="QN2.aspx.cs" Inherits="QN2.QN2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Q.N 2</title>
+    <link href="QN2.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container" runat="server">
+        <h1 runat="server">Simple Interest</h1>
+        <form id="form1" runat="server">
+            <div>
+                <asp:Label ID="lblPrincipal" Text="Principal" runat="server" />
+                <span runat="server" id="errPrincipal"></span>
+                <asp:TextBox ID="txtPrincipal" runat="server" TextMode="Number" />
+            </div>
+            <div>
+                <asp:Label ID="lblInterest" Text="Rate of Interest" runat="server" />
+                <span runat="server" id="errInterest"></span>
+                <asp:TextBox ID="txtInterest" runat="server" TextMode="Number" />
+            </div>
+            <div>
+                <asp:Label ID="lblTime" Text="Time" runat="server" />
+                <span runat="server" id="errTime"></span>
+                <asp:TextBox ID="txtTime" runat="server" TextMode="Number" />
+            </div>
+            <div>
+                <asp:Label ID="lblInterestAmount" Text="Interest Amount:" runat="server" />
+                <span id="lblInterestAmountTotal" runat="server"></span>
+            </div>
+            <div>
+                <asp:Label ID="lblTotalPlusInterest" Text="Total Plus Interest:" runat="server" />
+                <span id="lbllTotalPlusInterestTotal" runat="server"></span>
+            </div>
+            <div>
+                <asp:Button Text="Calculate" runat="server" ID="btnCalculate" OnClick="btnCalculate_Click" />
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ QN2/QN2/QN2.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QN2
+{
+    public partial class QN2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            errInterest.InnerHtml = string.Empty;
+            errTime.InnerHtml = string.Empty;
+            errPrincipal.InnerHtml = string.Empty;
+
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            string principal = txtPrincipal.Text;
+            string interest_rate = txtInterest.Text;
+            string time = txtTime.Text;
+
+            int error = 0;
+
+            if (string.IsNullOrWhiteSpace(txtPrincipal.Text))
+            {
+                error++;
+                errPrincipal.InnerHtml = "**Enter the valid value";
+            }
+            if (string.IsNullOrWhiteSpace(txtInterest.Text))
+            {
+                error++;
+                errInterest.InnerHtml = "** Enter the valid rate of interest";
+            }
+            if (string.IsNullOrWhiteSpace(txtTime.Text))
+            {
+                error++;
+                errTime.InnerHtml = "**Enter the valid time(in year)";
+            }
+            if (error == 0)
+            {
+                double dPrincipal = Convert.ToDouble(txtPrincipal.Text);
+                double dInterest_rate = Convert.ToDouble(txtInterest.Text);
+                double dTime = Convert.ToDouble(txtTime.Text);
+
+                double result = (dPrincipal * dTime * dInterest_rate) / 100;
+                lblInterestAmountTotal.InnerHtml = result.ToString();
+
+                double result_total = dPrincipal + result;
+                lbllTotalPlusInterestTotal.InnerHtml = result_total.ToString();
+            }
+
+
+        }
+    }
+}
