fbb819fe1eff3dbfe4f6b58c0fedd9b07314e595
mahendramahara <mahendramahara369@gmail.com>
2023-01-29 07:00:00 +0545

    Added solved ASP.NET project 🔥

---
+++ QN12/QN12/QN12.aspx
@@ -0,0 +1,104 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="QN12.aspx.cs" Inherits="QN12.QN12" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Q.N 12</title>
+    <link href="QN12.css" rel="stylesheet" />
+</head>
+<body>
+    <section class="container">
+        <h1>Information Update</h1>
+        <div class="errMessage-container">
+            <span id="errMessage" runat="server"></span>
+        </div>
+        <form id="form1" runat="server">
+            <div class="read">
+                <div class="message-container">
+                    <h5>Click the 'Retrieve Data' button to view the information about the books.</h5>
+                </div>
+                <div class="button-container">
+                    <asp:Button Text="Retrieve Data" runat="server" ID="btnRetrieve" OnClick="btnRetrieve_Click" />
+                </div>
+                <div class="message-container">
+                    <h5>Here display the data before it was updated.</h5>
+                </div>
+                <div class="table-container">
+                    <asp:GridView ID="GridViewBeforeUpdate" runat="server"></asp:GridView>
+                </div>
+            </div>
+            <div class="update">
+                <div class="message-container">
+                    <h5>Enter the old book ID you wish to edit.</h5>
+                </div>
+                <div class="editableID-container">
+                    <asp:Label Text="Enter Book ID:" runat="server" ID="lblBookID" />
+                    <asp:TextBox runat="server" ID="txtBookID" />
+                    <span id="errBookID" runat="server"></span>
+                </div>
+                <div class="message-container">
+                    <h5>Please fill out this form to update the data.</h5>
+                </div>
+                <div>
+                    <asp:Label Text="New Book ID:" runat="server" ID="lblNewID" />
+                    <asp:TextBox runat="server" ID="txtNewID" />
+                    <span id="errNewID" runat="server"></span>
+                </div>
+                <div>
+                    <asp:Label Text="Book Title:" runat="server" ID="lblTitle" />
+                    <asp:TextBox runat="server" ID="txtTitle" />
+                    <span id="errTitle" runat="server"></span>
+                </div>
+                <div>
+                    <asp:Label Text="Publisher:" runat="server" ID="lblPublisher" />
+                    <asp:TextBox runat="server" ID="txtPublisher" />
+                    <span id="errPublisher" runat="server"></span>
+                </div>
+                <div>
+                    <asp:Label Text="Author:" runat="server" ID="lblAuthor" />
+                    <asp:TextBox runat="server" ID="txtAuthor" />
+                    <span id="errAuthor" runat="server"></span>
+                </div>
+                <div>
+                    <asp:Label Text="Edition:" runat="server" ID="lblEdition" />
+                    <asp:TextBox runat="server" ID="txtEdition" />
+                    <span id="errEdition" runat="server"></span>
+                </div>
+                <div>
+                    <asp:Label Text="No of Pages:" runat="server" ID="lblPages" />
+                    <asp:TextBox runat="server" ID="txtPages" TextMode="Number" />
+                    <span id="errPages" runat="server"></span>
+                </div>
+                <div>
+                    <asp:Label Text="Price:" runat="server" ID="lblPrice" />
+                    <asp:TextBox runat="server" ID="txtPrice" TextMode="Number" />
+                    <span id="errPrice" runat="server"></span>
+                </div>
+                <div>
+                    <asp:Label Text="Publish Date:" runat="server" ID="lblPublishDate" />
+                    <asp:TextBox runat="server" ID="txtPublishDate" TextMode="Date" />
+                    <span id="errPublishDate" runat="server"></span>
+                </div>
+                <div>
+                    <asp:Label Text="ISBN No:" runat="server" ID="lblISBN" />
+                    <asp:TextBox runat="server" ID="txtISBN" />
+                    <span id="errISBN" runat="server"></span>
+                </div>
+                <div class="message-container">
+                    <h5>Click the 'Update Data' button to update the information about the book.</h5>
+                </div>
+                <div class="button-container">
+                    <asp:Button Text="Update Data" runat="server" ID="btnUpdate" OnClick="btnUpdate_Click" />
+                </div>
+                <div class="message-container">
+                    <h5>Here display the data after updated.</h5>
+                </div>
+                <div class="table-container">
+                    <asp:GridView ID="GridViewAfterUpdate" runat="server"></asp:GridView>
+                </div>
+            </div>
+        </form>
+    </section>
+</body>
+</html>
---
+++ QN12/QN12/QN12.aspx.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Security.Policy;
+
+namespace QN12
+{
+    public partial class QN12 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRetrieve_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection connect = new MySqlConnection("Server=localhost;Database=project;Uid=root;pwd=;");
+                connect.Open();
+
+                // SQL Query to retrieve all books
+                MySqlCommand command = new MySqlCommand("SELECT * FROM book_data", connect);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                // Display the data in GridView (you can use other controls based on your requirements)
+                if (dataTable.Rows.Count > 0)
+                {
+                    GridViewBeforeUpdate.DataSource = dataTable;
+                    GridViewBeforeUpdate.DataBind();
+                }
+                else
+                {
+                    errMessage.InnerText = "No data found.";
+                }
+            }
+            catch (Exception ex)
+            {
+                errMessage.InnerText = "Error retrieving books! **" + ex.Message;
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string selectedID = txtBookID.Text;
+
+            string bookID = txtNewID.Text;
+            string title = txtTitle.Text;
+            string publisher = txtPublisher.Text;
+            string author = txtAuthor.Text;
+            string edition = txtEdition.Text;
+            string pages = txtPages.Text;
+            string price = txtPrice.Text;
+            string publishDate = txtPublishDate.Text;
+            string isbn = txtISBN.Text;
+
+            int error = 0;
+
+            if (string.IsNullOrWhiteSpace(selectedID))
+            {
+                error++;
+                errBookID.InnerHtml = "**Enter the book ID first.";
+            }
+            if (string.IsNullOrWhiteSpace(bookID))
+            {
+                error++;
+                errNewID.InnerHtml = "**Enter the new book ID.";
+            }
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                error++;
+                errTitle.InnerHtml = "**Enter the new book title.";
+            }
+            if (string.IsNullOrWhiteSpace(publisher))
+            {
+                error++;
+                errPublisher.InnerHtml = "**Enter the new publisher name.";
+            }
+            if (string.IsNullOrWhiteSpace(author))
+            {
+                error++;
+                errAuthor.InnerHtml = "**Enter the new author name.";
+            }
+            if (string.IsNullOrWhiteSpace(edition))
+            {
+                error++;
+                errEdition.InnerHtml = "**Enter the edition.";
+            }
+            if (string.IsNullOrWhiteSpace(pages))
+            {
+                error++;
+                errPages.InnerHtml = "**Enter the total no of pages.";
+            }
+            if (string.IsNullOrWhiteSpace(price))
+            {
+                error++;
+                errPrice.InnerHtml = "**Enter the price of book.";
+            }
+            if (string.IsNullOrWhiteSpace(publishDate))
+            {
+                error++;
+                errPublishDate.InnerHtml = "**Choice the publish date of book.";
+            }
+            if (string.IsNullOrWhiteSpace(isbn))
+            {
+                error++;
+                errISBN.InnerHtml = "**Enter ISBN of the book.";
+            }
+            if (error == 0)
+            {
+                try
+                {
+                    MySqlConnection connect = new MySqlConnection("Server=localhost;Database=project;Uid=root;pwd=;");
+                    connect.Open();
+
+                    // Check if the book with the given ID exists
+                    MySqlCommand checkCommand = new MySqlCommand("SELECT COUNT(*) FROM book_data WHERE Book_ID = @bookid", connect);
+                    checkCommand.Parameters.AddWithValue("@bookid", selectedID);
+
+                    int bookCount = Convert.ToInt32(checkCommand.ExecuteScalar());
+
+                    if (bookCount > 0)
+                    {
+                        // Update operation
+                        MySqlCommand updateCommand = new MySqlCommand("UPDATE book_data SET Book_ID=@booknewID, Title = @title, Publisher = @publisher, Author = @author, Edition = @edition, Toatl_Pages = @pages, Price = @price, Publish_Date = @publishdate, ISBN = @isbn WHERE Book_ID = @bookid", connect);
+                        updateCommand.Parameters.AddWithValue("@bookid", selectedID);
+                        updateCommand.Parameters.AddWithValue("@booknewID", bookID);
+                        updateCommand.Parameters.AddWithValue("@title", title);
+                        updateCommand.Parameters.AddWithValue("@publisher", publisher);
+                        updateCommand.Parameters.AddWithValue("@author", author);
+                        updateCommand.Parameters.AddWithValue("@edition", edition);
+                        updateCommand.Parameters.AddWithValue("@pages", pages);
+                        updateCommand.Parameters.AddWithValue("@price", price);
+                        updateCommand.Parameters.AddWithValue("@publishdate", publishDate);
+                        updateCommand.Parameters.AddWithValue("@isbn", isbn);
+
+                        updateCommand.ExecuteNonQuery();
+                        errMessage.InnerText = "Database Connection and Record Updated Successfully";
+
+                        try
+                        {
+                            MySqlConnection connect1 = new MySqlConnection("Server=localhost;Database=project;Uid=root;pwd=;");
+                            connect1.Open();
+
+                            // SQL Query to retrieve all books
+                            MySqlCommand command1 = new MySqlCommand("SELECT * FROM book_data", connect1);
+                            MySqlDataAdapter adapter1 = new MySqlDataAdapter(command1);
+                            DataTable dataTable1 = new DataTable();
+                            adapter1.Fill(dataTable1);
+
+                            // Display the data in GridView (you can use other controls based on your requirements)
+                            if (dataTable1.Rows.Count > 0)
+                            {
+                                GridViewAfterUpdate.DataSource = dataTable1;
+                                GridViewAfterUpdate.DataBind();
+                            }
+                            else
+                            {
+                                errMessage.InnerText = "Error!";
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            errMessage.InnerText = "Error retrieving books! **" + ex.Message;
+                        }
+
+                    }
+                    else
+                    {
+                        errMessage.InnerText = "Error! Book with ID " + selectedID + " does not exist.";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    errMessage.InnerText = "Error! **" + ex.Message;
+                }
+            }
+        }
+    }
+}
