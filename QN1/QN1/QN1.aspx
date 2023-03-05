4e11836664626e4aa8ae33e4710cd5a7b19aa6a8
mahendramahara <mahendramahara369@gmail.com>
2023-03-05 07:00:00 +0545

    Added solved ASP.NET project 🔥

---
+++ QN1/QN1/QN1.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="QN1.aspx.cs" Inherits="QN1.QN1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Q.N 1</title>
+    <link href="QN1.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container" runat="server">
+        <form id="form1" runat="server">
+            <div>
+                <asp:TextBox ID="txtNum1" runat="server" textMode="Number" />
+                <span id="errNum1" runat="server"></span>
+            </div>
+            <div>
+                <asp:TextBox ID="txtNum2" runat="server" TextMode="Number" />
+                <span id="errNum2" runat="server"></span>
+            </div>
+            <div>
+                <span id="lblResult" runat="server">Enter number and click any button:</span>
+            </div>
+            <div id="buttons">
+                <asp:Button ID="btnAdd" Text="Add" runat="server" OnClick="btnAdd_Click" />
+                <asp:Button ID="btnSub" Text="Subtract" runat="server" OnClick="btnSub_Click" />
+                <asp:Button ID="btnDiv" Text="Divide" runat="server" OnClick="btnDiv_Click" />
+                <asp:Button ID="btnMul" Text="Multiply" runat="server" OnClick="btnMul_Click" />
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ QN1/QN1/QN1.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace QN1
+{
+    public partial class QN1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            errNum1.InnerHtml = String.Empty;
+            errNum2.InnerHtml = String.Empty;
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (ValidateInput())
+            {
+                int addNumber1 = Convert.ToInt32(txtNum1.Text);
+                int addNumber2 = Convert.ToInt32(txtNum2.Text);
+                int addResult = addNumber1 + addNumber2;
+                lblResult.InnerText = "The SUM: " + addResult;
+            }
+        }
+
+        protected void btnSub_Click(object sender, EventArgs e)
+        {
+            if (ValidateInput())
+            {
+                int subNumber1 = Convert.ToInt32(txtNum1.Text);
+                int subNumber2 = Convert.ToInt32(txtNum2.Text);
+                int subResult = subNumber1 - subNumber2;
+                lblResult.InnerText = "Subtract from Num1 - Num2: " + subResult;
+            }
+        }
+
+        protected void btnDiv_Click(object sender, EventArgs e)
+        {
+            if (ValidateInput())
+            {
+                int divNumber1 = Convert.ToInt32(txtNum1.Text);
+                int divNumber2 = Convert.ToInt32(txtNum2.Text);
+                float divResult = divNumber1 / divNumber2;
+                lblResult.InnerText = "Division Num1 / Num2: " + divResult;
+            }
+        }
+
+        protected void btnMul_Click(object sender, EventArgs e)
+        {
+            if (ValidateInput())
+            {
+                int mulNumber1 = Convert.ToInt32(txtNum1.Text);
+                int mulNumber2 = Convert.ToInt32(txtNum2.Text);
+                int mulResult = mulNumber1 * mulNumber2;
+                lblResult.InnerText = "Multiplication: " + mulResult;
+            }
+        }
+
+        private bool ValidateInput()
+        {
+            int error = 0;
+
+            if (String.IsNullOrWhiteSpace(txtNum1.Text))
+            {
+                error++;
+                errNum1.InnerHtml = "Enter Valid First Number";
+            }
+            else
+            {
+                errNum1.InnerHtml = "";
+            }
+
+            if (String.IsNullOrWhiteSpace(txtNum2.Text))
+            {
+                error++;
+                errNum2.InnerHtml = "Enter Valid Second Number";
+            }
+            else
+            {
+                errNum2.InnerHtml = "";
+            }
+
+            return error == 0;
+        }
+    }
+}
