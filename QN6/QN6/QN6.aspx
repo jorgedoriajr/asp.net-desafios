75498c8b872a94b0a27186a9167d2a6ef77978d3
mahendramahara <mahendramahara369@gmail.com>
2023-04-04 07:00:00 +0545

    Added solved ASP.NET project 🔥

---
+++ QN6/QN6/QN6.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="QN6.aspx.cs" Inherits="QN6.QN6" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Q.N 6</title>
+    <link href="QN6.css" rel="stylesheet" />
+</head>
+<body>
+    <section class="container">
+        <h1>Login from DataBase</h1>
+        <form id="form1" runat="server">
+            <div class="content">
+                <asp:Label Text="Email:" runat="server" ID="lblEmail" />
+                <asp:TextBox runat="server" ID="txtEmail" TextMode="Email" />
+                <span id="errEmail" runat="server"></span>
+            </div>
+            <div class="content">
+                <asp:Label Text="Password:" runat="server" ID="lblPassword" />
+                <asp:TextBox runat="server" ID="txtPassword" TextMode="Password" />
+                <span id="errPassword" runat="server"></span>
+            </div>
+            <div class="content">
+                <asp:Button Text="Login" runat="server" ID="btnLogin" OnClick="btnLogin_Click" />
+            </div>
+            <div>
+                <span id="errDatabase" runat="server"></span>
+            </div>
+        </form>
+    </section>
+</body>
+</html>
---
+++ QN6/QN6/QN6.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Lifetime;
+using System.Security.Policy;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using MySql.Data.MySqlClient;
+
+namespace QN6
+{
+    public partial class QN6 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ClearErrors();
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            ClearErrors();
+
+            string email = txtEmail.Text;
+            string password = txtPassword.Text;
+
+            int error = 0;
+
+            if (string.IsNullOrWhiteSpace(email)) 
+            {
+                error++;
+                errEmail.InnerHtml = "**Please Enter Your Email ID.";
+            }
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                error++;
+                errPassword.InnerHtml = "**Please Enter Your Password.";
+            }
+
+            if (error == 0)
+            {
+                try
+                {
+                    //Database Connectivety
+                    MySqlConnection connect = new MySqlConnection("Server=localhost;Database=project;Uid=root;pwd=;");
+                    connect.Open();
+
+                    //Sql Query To Record
+                    MySqlCommand command = new MySqlCommand("SELECT COUNT(*) FROM registrations WHERE email = @email AND password = @password", connect);
+                    command.Parameters.AddWithValue("@email", email);
+                    command.Parameters.AddWithValue("@password", password);
+
+                    // Execute the query and check the result
+                    int count = Convert.ToInt32(command.ExecuteScalar());
+                    if (count > 0)
+                    {
+                        // Successful login
+                        Response.Redirect("dashboard.html");
+                    }
+                    else
+                    {
+                        // Invalid login
+                        errDatabase.InnerHtml = "**Invalid Email or Password.";
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    errDatabase.InnerText = "Error! " + ex.Message;
+                }
+
+            }
+        }
+
+        private void ClearErrors()
+        {
+            errEmail.InnerHtml = String.Empty;
+            errPassword.InnerHtml = String.Empty;
+        }
+    }
+}
