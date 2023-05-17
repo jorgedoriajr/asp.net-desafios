13bbdef3a4c72ec0f104560dff18b072bc1578d6
mahendramahara <mahendramahara369@gmail.com>
2023-05-17 07:00:00 +0545

    Added solved ASP.NET project 🔥

---
+++ QN3/QN3/QN3.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="QN3.aspx.cs" Inherits="QN3.QN3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Q.N 3</title>
+    <link href="QN3.css" rel="stylesheet" />
+</head>
+<body>
+    <div runat="server" class="container">
+        <form id="form1" runat="server">
+            <div class="top-section">
+                <span runat="server">Assessement Info</span>
+                <div runat="server" id="selectGender">
+                    <asp:DropDownList runat="server" ID="drlGender">
+                        <asp:ListItem Text="Select Gender" />
+                        <asp:ListItem Text="Male" Value="male" />
+                        <asp:ListItem Text="Female" Value="female" />
+                        <asp:ListItem Text="Others" Value="others" />
+                    </asp:DropDownList>
+                </div>
+                <div runat="server" id="selectStatus">
+                    <asp:DropDownList runat="server" ID="drlStatus">
+                        <asp:ListItem Text="Select Status" />
+                        <asp:ListItem Text="Married" Value="married" />
+                        <asp:ListItem Text="Un-Married" Value="unmarried" />
+                    </asp:DropDownList>
+                </div>
+            </div>
+            <div class="firstInput">
+                <asp:Label Text="Monthly Salary" runat="server" ID="lblMonthlySalary" />
+                <asp:TextBox runat="server" ID="txtMonthlySalary" TextMode="Number" />
+            </div>
+            <div class="annualCalculation">
+                <asp:Label Text="Annual Income" runat="server" ID="lblAnnualIncome" />
+                <span id="lblAnnualCalculated" runat="server"></span>
+            </div>
+            <div class="secondInput">
+                <asp:Label Text="Annual Gross Salary" runat="server" ID="lblGrossSalary" />
+                <asp:TextBox runat="server" ID="txtGrossSalary" TextMode="Number" />
+            </div>
+            <div class="buttonSection">
+                <asp:Button Text="Clear Inputs" runat="server" ID="btnClearInput" OnClick="btnClearInput_Click" />
+                <asp:Button Text="Calculate Tax" runat="server" ID="btnCalculateTax" OnClick="btnCalculateTax_Click" />
+            </div>
+        </form>
+        <div class="resultSection">
+            <span runat="server" id="calculatedTax">Calculated Tax:</span>
+            <span runat="server" id="lblResult"></span>
+        </div>
+        <div class="errMessage">
+            <h6 id="errMsg1" runat="server" ></h6>
+            <h6 id="errMsg2" runat="server" ></h6>
+            <h6 id="errMsg3" runat="server" ></h6>
+            <h6 id="errMsg4" runat="server" ></h6>
+        </div>
+    </div>
+</body>
+</html>
---
+++ QN3/QN3/QN3.aspx.cs
@@ -0,0 +1,202 @@
+﻿using System;
+
+namespace QN3
+{
+    public partial class QN3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            errMsg1.InnerHtml = String.Empty;
+            errMsg2.InnerHtml = String.Empty;
+            errMsg3.InnerHtml = String.Empty;
+            errMsg4.InnerHtml = String.Empty;
+            lblAnnualCalculated.InnerHtml = String.Empty;
+            lblResult.InnerHtml = String.Empty;
+        }
+
+        protected void btnClearInput_Click(object sender, EventArgs e)
+        {
+            drlGender.SelectedIndex = 0;
+            drlStatus.SelectedIndex = 0;
+            txtMonthlySalary.Text = string.Empty;
+            txtGrossSalary.Text = string.Empty;
+            errMsg1.InnerHtml = String.Empty;
+            errMsg2.InnerHtml = String.Empty;
+            errMsg3.InnerHtml = String.Empty;
+            errMsg4.InnerHtml = String.Empty;
+            lblAnnualCalculated.InnerHtml = String.Empty;
+            lblResult.InnerHtml = String.Empty;
+        }
+
+        protected void btnCalculateTax_Click(object sender, EventArgs e)
+        {
+            string selectedGender = drlGender.SelectedValue;
+            string selectedStatus = drlStatus.SelectedValue;
+            string monthlySalary = txtMonthlySalary.Text;
+            string annualIncome = txtGrossSalary.Text;
+
+            int error = 0;
+
+            double annualIncomeCalculated = 0;
+            double yearlyInncomeConverted = 0;
+            double finalIncomeCalc = 0;
+            double calculatedTax = 0;
+
+            if (drlGender.SelectedIndex == 0)
+            {
+                error++;
+                errMsg1.InnerHtml = "**Please select a gender.";
+            }
+            if(drlStatus.SelectedIndex == 0)
+            {
+                error++;
+                errMsg2.InnerHtml = "**Please select merried status.";
+            }
+            if (string.IsNullOrWhiteSpace(txtMonthlySalary.Text) && string.IsNullOrWhiteSpace(txtGrossSalary.Text))
+            {
+                error++;
+                errMsg3.InnerHtml = "**Please insert valid value in any one box.";
+            }
+
+            if (!string.IsNullOrWhiteSpace(txtMonthlySalary.Text)) 
+            { 
+                double monthlySalaryConverted = Convert.ToDouble(txtMonthlySalary.Text);
+                annualIncomeCalculated = monthlySalaryConverted * 12;
+                lblAnnualCalculated.InnerHtml = "as Calculated: " + annualIncomeCalculated;
+            }
+            if (!string.IsNullOrWhiteSpace(txtGrossSalary.Text))
+            {
+                yearlyInncomeConverted = Convert.ToDouble(txtGrossSalary.Text);
+            }
+            if(!string.IsNullOrWhiteSpace(txtMonthlySalary.Text) && !string.IsNullOrWhiteSpace(txtGrossSalary.Text))
+            {
+                if(annualIncomeCalculated != yearlyInncomeConverted)
+                {
+                    error++;
+                    errMsg4.InnerHtml = "**Annual income calculated and Gross salary should be same";
+                }
+                else if(annualIncomeCalculated == yearlyInncomeConverted)
+                {
+                    finalIncomeCalc = annualIncomeCalculated;
+                }
+            }
+            else if(!string.IsNullOrWhiteSpace(txtMonthlySalary.Text) && string.IsNullOrWhiteSpace(txtGrossSalary.Text))
+            {
+                finalIncomeCalc = annualIncomeCalculated;
+            }
+            else if(string.IsNullOrWhiteSpace(txtMonthlySalary.Text) && !string.IsNullOrWhiteSpace(txtGrossSalary.Text))
+            {
+                finalIncomeCalc = yearlyInncomeConverted;
+            }
+            if (error == 0)
+            {
+
+                if (selectedStatus== "married")
+                {
+                    if (finalIncomeCalc <= 450000)
+                    {
+                        calculatedTax = (finalIncomeCalc * 1) / 100;
+                    }
+                    else if (finalIncomeCalc <= 550000)
+                    {
+                        double remAmt = finalIncomeCalc-450000;
+                        double for450 = (450000 * 1) / 100;
+                        double forRemTax = (remAmt * 10) / 100;
+                        calculatedTax = for450 + forRemTax;
+                    }
+                    else if (finalIncomeCalc <= 750000)
+                    {
+                        double remAmt = finalIncomeCalc - 450000;
+                        double for450 = (450000 * 1) / 100;
+                        double remAmt2 = remAmt - 100000;
+                        double for550 = (100000 * 10) / 100;
+                        double forRemTax = (remAmt2 * 20) / 100;
+                        calculatedTax = for450 + for550 + forRemTax;
+                    }
+                    else if (finalIncomeCalc <= 1300000)
+                    {
+                        double remAmt = finalIncomeCalc - 450000;
+                        double for450 = (450000 * 1) / 100;
+                        double remAmt2 = remAmt - 100000;
+                        double for550 = (100000 * 10) / 100;
+                        double remAmt3 = remAmt2 - 200000;
+                        double for750 = (200000 * 20) / 100;
+                        double forRemTax = (remAmt3 * 30) / 100;
+                        calculatedTax = for450 + for550 + for750 + forRemTax;
+                    }
+                    else if (finalIncomeCalc > 1300000)
+                    {
+                        double remAmt = finalIncomeCalc - 450000;
+                        double for450 = (450000 * 1) / 100;
+                        double remAmt2 = remAmt - 100000;
+                        double for550 = (100000 * 10) / 100;
+                        double remAmt3 = remAmt2 - 200000;
+                        double for750 = (200000 * 20) / 100;
+                        double remAmt4 = remAmt3 - 550000;
+                        double for1300 = (550000 * 30) / 100;
+                        double forRemTax = (remAmt4 * 35) / 100;
+                        calculatedTax = for450 + for550 + for750 + for1300 + forRemTax;
+                    }
+                }
+                else if(selectedStatus == "unmarried")
+                {
+                    if (finalIncomeCalc <= 400000)
+                    {
+                        calculatedTax = (finalIncomeCalc * 1) / 100;
+                    }
+                    else if (finalIncomeCalc <= 500000)
+                    {
+                        double remAmt = finalIncomeCalc - 400000;
+                        double for400 = (400000 * 1) / 100;
+                        double forRemTax = (remAmt * 10) / 100;
+                        calculatedTax = for400 + forRemTax;
+                    }
+                    else if (finalIncomeCalc <= 750000)
+                    {
+                        double remAmt = finalIncomeCalc - 400000;
+                        double for400 = (400000 * 1) / 100;
+                        double remAmt2 = remAmt - 100000;
+                        double for500 = (100000 * 10) / 100;
+                        double forRemTax = (remAmt2 * 20) / 100;
+                        calculatedTax = for400 + for500 + forRemTax;
+                    }
+                    else if (finalIncomeCalc <= 1300000)
+                    {
+                        double remAmt = finalIncomeCalc - 400000;
+                        double for400 = (400000 * 1) / 100;
+                        double remAmt2 = remAmt - 100000;
+                        double for500 = (100000 * 10) / 100;
+                        double remAmt3 = remAmt2 - 250000;
+                        double for750 = (250000 * 20) / 100;
+                        double forRemTax = (remAmt3 * 30) / 100;
+                        calculatedTax = for400 + for500 + for750 + forRemTax;
+                    }
+                    else if (finalIncomeCalc > 1300000)
+                    {
+                        double remAmt = finalIncomeCalc - 400000;
+                        double for400 = (400000 * 1) / 100;
+                        double remAmt2 = remAmt - 100000;
+                        double for500 = (100000 * 10) / 100;
+                        double remAmt3 = remAmt2 - 250000;
+                        double for750 = (250000 * 20) / 100;
+                        double remAmt4 = remAmt3 - 550000;
+                        double for1300 = (550000 * 30) / 100;
+                        double forRemTax = (remAmt4 * 35) / 100;
+                        calculatedTax = for400 + for500 + for750 + for1300 + forRemTax;
+                    }
+                }
+                if(selectedGender == "female")
+                {
+                    double lessTax = (calculatedTax * 10) / 100;
+                    calculatedTax = calculatedTax - lessTax ;
+                }
+                else if (selectedGender == "others")
+                {
+                    double lessTax = (calculatedTax * 15) / 100;
+                    calculatedTax = calculatedTax - lessTax;
+                }
+                lblResult.InnerHtml = calculatedTax.ToString();
+            }
+        }
+    }
+}
