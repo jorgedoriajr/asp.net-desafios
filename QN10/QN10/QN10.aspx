1e8862e4245e6fd55039456a36df446ae7f81ab4
mahendramahara <mahendramahara369@gmail.com>
2023-06-22 07:00:00 +0545

    Added solved ASP.NET project 🔥

---
+++ QN10/QN10/QN10.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="QN10.aspx.cs" Inherits="QN10.QN10" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Q.N 10</title>
+    <link href="QN10.css" rel="stylesheet" />
+</head>
+<body>
+    <section class="container">
+        <h1>Book Record Creation</h1>
+        <div class="error-container">
+            <span id="errMessage" runat="server"></span>
+        </div>
+        <form id="form1" runat="server">
+            <div>
+                <asp:Label Text="Book ID:" runat="server" ID="lblBookID" />
+                <asp:TextBox runat="server" ID="txtBookID" />
+                <span id="errBookID" runat="server"></span>
+            </div>
+            <div>
+                <asp:Label Text="Book Title:" runat="server" ID="lblTitle" />
+                <asp:TextBox runat="server" ID="txtTitle" />
+                <span id="errTitle" runat="server"></span>
+            </div>
+            <div>
+                <asp:Label Text="Publisher:" runat="server" ID="lblPublisher" />
+                <asp:TextBox runat="server" ID="txtPublisher" />
+                <span id="errPublisher" runat="server"></span>
+            </div>
+            <div>
+                <asp:Label Text="Author:" runat="server" ID="lblAuthor" />
+                <asp:TextBox runat="server" ID="txtAuthor" />
+                <span id="errAuthor" runat="server"></span>
+            </div>
+            <div>
+                <asp:Label Text="Edition:" runat="server" ID="lblEdition" />
+                <asp:TextBox runat="server" ID="txtEdition" />
+                <span id="errEdition" runat="server"></span>
+            </div>
+            <div>
+                <asp:Label Text="No of Pages:" runat="server" ID="lblPages" />
+                <asp:TextBox runat="server" ID="txtPages" TextMode="Number" />
+                <span id="errPages" runat="server"></span>
+            </div>
+            <div>
+                <asp:Label Text="Price:" runat="server" ID="lblPrice" />
+                <asp:TextBox runat="server" ID="txtPrice" TextMode="Number" />
+                <span id="errPrice" runat="server"></span>
+            </div>
+            <div>
+                <asp:Label Text="Publish Date:" runat="server" ID="lblPublishDate" />
+                <asp:TextBox runat="server" ID="txtPublishDate" TextMode="Date" />
+                <span id="errPublishDate" runat="server"></span>
+            </div>
+            <div>
+                <asp:Label Text="ISBN No:" runat="server" ID="lblISBN" />
+                <asp:TextBox runat="server" ID="txtISBN" />
+                <span id="errISBN" runat="server"></span>
+            </div>
+            <div class="button-container">
+                <asp:Button Text="Submit" runat="server" ID="btnSubmit" OnClick="btnSubmit_Click" />
+            </div>
+        </form>
+
+    </section>
+</body>
+</html>
---
+++ QN10/QN10/QN10.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using MySql.Data.MySqlClient;
+
+namespace QN10
+{
+    public partial class QN10 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ResetErrorMessages();
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            ResetErrorMessages();
+
+            string bookID = txtBookID.Text;
+            string title = txtTitle.Text;
+            string publisher = txtPublisher.Text;
+            string author = txtAuthor.Text;
+            string edition = txtEdition.Text;
+            string pages = txtPages.Text;
+            string price = txtPrice.Text;
+            string publishDate = txtPublishDate.Text;
+            string isbn = txtISBN.Text;
+
+            int error = 0;
+
+            if (string.IsNullOrWhiteSpace(bookID))
+            {
+                error++;
+                errBookID.InnerHtml = "**Enter the book ID.";
+            }
+            if(string.IsNullOrWhiteSpace(title))
+            {
+                error++;
+                errTitle.InnerHtml = "**Enter the book title.";
+            }
+            if(string.IsNullOrWhiteSpace(publisher))
+            {
+                error++;
+                errPublisher.InnerHtml = "**Enter the publisher name.";
+            }
+            if (string.IsNullOrWhiteSpace(author))
+            {
+                error++;
+                errAuthor.InnerHtml = "**Enter the author name.";
+            }
+            if (string.IsNullOrWhiteSpace(edition))
+            {
+                error++;
+                errEdition.InnerHtml = "**Enter the edition.";
+            }
+            if (string.IsNullOrWhiteSpace(pages))
+            {
+                error++;
+                errPages.InnerHtml = "**Enter the total no of pages.";
+            }
+            if(string.IsNullOrWhiteSpace(price))
+            {
+                error++;
+                errPrice.InnerHtml = "**Enter the price of book.";
+            }
+            if (string.IsNullOrWhiteSpace(publishDate))
+            {
+                error++;
+                errPublishDate.InnerHtml = "**Choice the publish date of book.";
+            }
+            if (string.IsNullOrWhiteSpace(isbn))
+            {
+                error++;
+                errISBN.InnerHtml = "**Enter ISBN of the book.";
+            }
+
+            if (error == 0)
+            {
+                try
+                {
+                    //Database Connectivety
+                    MySqlConnection connect = new MySqlConnection("Server=localhost;Database=project;Uid=root;pwd=;");
+                    connect.Open();
+
+                    //Sql Query To Record
+                    MySqlCommand command = new MySqlCommand("INSERT INTO book_data (Book_ID, Title, Publisher, Author, Edition, Toatl_Pages, Price, Publish_Date, ISBN)\r\nVALUES (@bookid, @title, @publisher, @author, @edition, @pages, @price, @publishdate, @isbn)", connect);
+                    command.Parameters.AddWithValue("@bookid", bookID);
+                    command.Parameters.AddWithValue("@title", title);
+                    command.Parameters.AddWithValue("@publisher", publisher);
+                    command.Parameters.AddWithValue("@author", author);
+                    command.Parameters.AddWithValue("@edition", edition);
+                    command.Parameters.AddWithValue("@pages", pages);
+                    command.Parameters.AddWithValue("@price", price);
+                    command.Parameters.AddWithValue("@publishdate", publishDate);
+                    command.Parameters.AddWithValue("@isbn", isbn);
+
+                    //execute query
+                    command.ExecuteNonQuery();
+                    errMessage.InnerText = "Database Connection and Record Stored Successfully";
+
+                }
+                catch (Exception ex)
+                {
+                    errMessage.InnerText = "Error! **" + ex.Message;
+                }
+            }
+        }
+        private void ResetErrorMessages()
+        {
+            errBookID.InnerHtml = String.Empty;
+            errTitle.InnerHtml = String.Empty;
+            errPublisher.InnerHtml = String.Empty;
+            errAuthor.InnerHtml = String.Empty;
+            errEdition.InnerHtml = String.Empty;
+            errPages.InnerHtml = String.Empty;
+            errPrice.InnerHtml = String.Empty;
+            errPublishDate.InnerHtml = String.Empty;
+            errISBN.InnerHtml = String.Empty;
+        }
+    }
+}
