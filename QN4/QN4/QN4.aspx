9efef4b91d45334f855a74439b7b9ee0cc3878f3
mahendramahara <mahendramahara369@gmail.com>
2023-07-27 07:00:00 +0545

    Added solved ASP.NET project 🔥

---
+++ QN4/QN4/QN4.aspx
@@ -0,0 +1,116 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="QN4.aspx.cs" Inherits="QN4.QN4" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Q.N 4</title>
+    <link href="QN4.css  " rel="stylesheet" />
+</head>
+<body>
+    <div class="container" runat="server">
+        <div class="title-container">
+            <h1>Student Registration Form </h1>
+        </div>
+        <form id="form1" runat="server">
+            <div class="form-content">
+                <asp:Label Text="Name:" runat="server" ID="lblName" />
+                <asp:TextBox placeholder="Enter full name" ID="txtName" runat="server" />
+                <span id="errName" runat="server"></span>
+            </div>
+            <div class="form-content">
+                <asp:Label Text="Father's Name:" runat="server" ID="lblFatherName" />
+                <asp:TextBox ID="txtFatherName" runat="server" />
+                <span id="errFatherName" runat="server"></span>
+            </div>
+            <div class="form-content">
+                <asp:Label Text="Mother's Name:" runat="server" ID="lblMotherName" />
+                <asp:TextBox ID="txtMotherName" runat="server" />
+                <span id="errMotherName" runat="server"></span>
+            </div>
+            <div class="form-content">
+                <asp:Label Text="Phone Number:" runat="server" ID="lblPhoneNo" />
+                <asp:TextBox TextMode="Phone" placeholder="017XXXXXXXXXX" ID="txtPhoneNo" runat="server" />
+                <span id="errPhoneNo" runat="server"></span>
+            </div>
+            <div class="form-content">
+                <asp:Label Text="Email:" runat="server" ID="lblEmail" />
+                <asp:TextBox TextMode="Email" ID="txtEmail" runat="server" />
+                <span id="errEmail" runat="server"></span>
+            </div>
+            <div class="form-content">
+                <asp:Label Text="Gender:" runat="server" ID="lblGender" />
+                <div class="radioOption">
+                    <asp:RadioButton ID="rblMale" runat="server" Text="Male" Value="Male" GroupName="rblGender" />
+                    <asp:RadioButton ID="rblFemale" runat="server" Text="Female" Value="Female" GroupName="rblGender" />
+                    <asp:RadioButton ID="rblOther" runat="server" Text="Other" Value="Other" GroupName="rblGender" />
+                </div>
+                <span id="errGender" runat="server"></span>
+            </div>
+            <div class="form-content">
+                <asp:Label Text="Date Of Birth:" runat="server" ID="lblDob" />
+                <div class="dateOption">
+                    <asp:TextBox ID="txtDay" runat="server" MaxLength="2" Width="30"></asp:TextBox>
+                    -
+                    <asp:TextBox ID="txtMonth" runat="server" MaxLength="2" Width="30"></asp:TextBox>
+                    -
+                    <asp:TextBox ID="txtYear" runat="server" MaxLength="4" Width="30"></asp:TextBox>
+                    <asp:Label Text="(dd-mm-yy)" runat="server" ID="lblDobRef" />
+                </div>
+                <span id="errDob" runat="server"></span>
+            </div>
+            <div class="form-content">
+                <asp:Label Text="Address:" runat="server" ID="lblAddress" />
+                <asp:TextBox placeholder="Street:-     House:-      Road:-" ID="txtAddress" runat="server" />
+                <span id="errAddress" runat="server"></span>
+            </div>
+            <div class="form-content">
+                <asp:Label Text="Blood Group:" runat="server" ID="lblBloodGroup" />
+                <asp:DropDownList ID="ddlBloodGroup" runat="server">
+                    <asp:ListItem Text="Select" />
+                    <asp:ListItem Text="A+" Value="A+" />
+                    <asp:ListItem Text="A-" Value="A-" />
+                    <asp:ListItem Text="B+" Value="B+" />
+                    <asp:ListItem Text="B-" Value="B-" />
+                    <asp:ListItem Text="AB+" Value="AB+" />
+                    <asp:ListItem Text="AB-" Value="AB-" />
+                    <asp:ListItem Text="O+" Value="O+" />
+                    <asp:ListItem Text="O-" Value="O-" />
+                </asp:DropDownList>
+                <span id="errBloodGroup" runat="server"></span>
+            </div>
+            <div class="form-content">
+                <asp:Label Text="Department:" runat="server" ID="lblDepartment" />
+                <div class="radioOption">
+                    <asp:RadioButton ID="lblCSE" runat="server" Text="CSE" Value="CSE" GroupName="rblDepartment" />
+                    <asp:RadioButton ID="lblEEE" runat="server" Text="EEE" Value="EEE" GroupName="rblDepartment" />
+                    <asp:RadioButton ID="lblBBA" runat="server" Text="BBA" Value="BBA" GroupName="rblDepartment" />
+                </div>
+                <span id="errDepartment" runat="server"></span>
+            </div>
+            <div class="form-content">
+                <asp:Label Text="Course:" runat="server" ID="lblCourse" />
+                <div class="radioOption">
+                    <asp:RadioButton ID="lblC" runat="server" Text="C" Value="C" GroupName="rblCourse" />
+                    <asp:RadioButton ID="lblCpp" runat="server" Text="C++" Value="C++" GroupName="rblCourse" />
+                    <asp:RadioButton ID="lblJava" runat="server" Text="Java" Value="Java" GroupName="rblCourse" />
+                    <asp:RadioButton ID="lblAI" runat="server" Text="AI" Value="AI" GroupName="rblCourse" />
+                    <asp:RadioButton ID="lblMachineLearning" runat="server" Text="Machine Learning" Value="ML" GroupName="rblCourse" />
+                    <asp:RadioButton ID="lblRobotics" runat="server" Text="Robotics" Value="Robotics" GroupName="rblCourse" />
+                </div>
+                <span id="errCourse" runat="server"></span>
+            </div>
+            <div class="form-content">
+                <asp:Label Text="Photo:" runat="server" ID="lblPhoto" />
+                <asp:FileUpload ID="filePhoto" runat="server" />
+                <span id="errPhoto" runat="server"></span>
+            </div>
+            <div class="form-content">
+                <asp:Button Text="Submit" runat="server" ID="btnSubmit" OnClick="btnSubmit_Click" />
+                <asp:Button Text="Reset" runat="server" ID="btnReset" OnClick="btnReset_Click" />
+                <span id="errButton" runat="server"></span>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ QN4/QN4/QN4.aspx.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QN4
+{
+    public partial class QN4 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            errName.InnerHtml = string.Empty;
+            errFatherName.InnerHtml = string.Empty;
+            errMotherName.InnerHtml = string.Empty;
+            errPhoneNo.InnerHtml = string.Empty;
+            errEmail.InnerHtml = string.Empty;
+            errGender.InnerHtml = string.Empty;
+            errDob.InnerHtml = string.Empty;
+            errAddress.InnerHtml = string.Empty;
+            errBloodGroup.InnerHtml = string.Empty;
+            errDepartment.InnerHtml = string.Empty;
+            errCourse.InnerHtml = string.Empty;
+            errPhoto.InnerHtml = string.Empty;
+            errButton.InnerHtml = string.Empty;
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string fname = txtFatherName.Text;
+            string mname = txtMotherName.Text;
+            string phone = txtPhoneNo.Text;
+            string email = txtEmail.Text;
+            string gender;
+            if (rblMale.Checked)
+            {
+                gender = rblMale.Text;
+            }
+            else if (rblFemale.Checked)
+            {
+                gender = rblFemale.Text;
+            }
+            else if (rblOther.Checked)
+            {
+                gender = rblOther.Text;
+            }
+            else
+            {
+                gender = string.Empty;
+            }
+            string day = txtDay.Text;
+            string month = txtMonth.Text;
+            string year = txtYear.Text;
+            string address = txtAddress.Text;
+            string bloodGroup = ddlBloodGroup.SelectedValue;
+            string SelectedDepartment;
+            if (lblCSE.Checked)
+            {
+                SelectedDepartment = lblCSE.Text;
+            }
+            else if (lblEEE.Checked)
+            {
+                SelectedDepartment = lblEEE.Text;
+            }
+            else if (lblBBA.Checked)
+            {
+                SelectedDepartment = lblBBA.Text;
+            }
+            else
+            {
+                SelectedDepartment = string.Empty;
+            }
+
+            string SelectedCourse;
+            if (lblC.Checked)
+            {
+                SelectedCourse = lblC.Text;
+            }
+            else if (lblCpp.Checked)
+            {
+                SelectedCourse = lblCpp.Text;
+            }
+            else if (lblJava.Checked)
+            {
+                SelectedCourse = lblJava.Text;
+            }
+            else if (lblAI.Checked)
+            {
+                SelectedCourse = lblAI.Text;
+            }
+            else if (lblMachineLearning.Checked)
+            {
+                SelectedCourse = lblMachineLearning.Text;
+            }
+            else if (lblRobotics.Checked)
+            {
+                SelectedCourse = lblRobotics.Text;
+            }
+            else
+            {
+                SelectedCourse = string.Empty;
+            }
+
+            int error = 0;
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                error++;
+                errName.InnerHtml = "**Enter your Name";
+            }
+            if (string.IsNullOrWhiteSpace(fname))
+            {
+                error++;
+                errFatherName.InnerHtml = "**Enter your father Name.";
+            }
+            if (string.IsNullOrWhiteSpace(mname))
+            {
+                error++;
+                errMotherName.InnerHtml = "**Enter your mother Name.";
+            }
+            if (string.IsNullOrWhiteSpace(phone))
+            {
+                error++;
+                errPhoneNo.InnerHtml = "**Enter your phone number.";
+            }
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                error++;
+                errEmail.InnerHtml = "**Enter your email address";
+            }
+            if (string.IsNullOrWhiteSpace(gender))
+            {
+                error++;
+                errGender.InnerHtml = "**Select your gender.";
+            }
+            if (true)
+            {
+                if (string.IsNullOrWhiteSpace(day))
+                {
+                    error++;
+                    errDob.InnerHtml = "**Fill-up 3 field properly.";
+                }
+                if (string.IsNullOrWhiteSpace(month))
+                {
+                    error++;
+                    errDob.InnerHtml = "**Fill-up 3 field properly.";
+                }
+                if (string.IsNullOrWhiteSpace(year))
+                {
+                    error++;
+                    errDob.InnerHtml = "**Fill-up 3 field properly.";
+                }
+
+            }
+            if (string.IsNullOrWhiteSpace(address))
+            {
+                error++;
+                errAddress.InnerHtml = "**Enter your address.";
+            }
+            if (string.IsNullOrWhiteSpace(bloodGroup))
+            {
+                error++;
+                errBloodGroup.InnerText = "**Select your blood group.";
+            }
+            if (string.IsNullOrWhiteSpace(SelectedDepartment))
+            {
+                error++;
+                errDepartment.InnerHtml = "**Select your Depertment.";
+            }
+            if (string.IsNullOrWhiteSpace(SelectedCourse))
+            {
+                error++;
+                errCourse.InnerHtml = "**Select your Course.";
+            }
+
+            if (!filePhoto.HasFile)
+            {
+                error++;
+                errPhoto.InnerText = "**Please select a photo.";
+            }
+            if (error == 0)
+            {
+                errButton.InnerHtml = "**Good Job!";
+            }
+        }
+
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            txtName.Text = string.Empty;
+            txtFatherName.Text = string.Empty;
+            txtMotherName.Text = string.Empty;
+            txtPhoneNo.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            rblMale.Checked = false;
+            rblFemale.Checked = false;
+            rblOther.Checked = false;
+            txtDay.Text = string.Empty;
+            txtMonth.Text = string.Empty;
+            txtYear.Text = string.Empty;
+            txtAddress.Text = string.Empty;
+            ddlBloodGroup.SelectedIndex = 0;
+            lblCSE.Checked = false;
+            lblEEE.Checked = false;
+            lblBBA.Checked = false;
+            lblC.Checked = false;
+            lblCpp.Checked = false;
+            lblJava.Checked = false;
+            lblAI.Checked = false;
+            lblMachineLearning.Checked = false;
+            lblRobotics.Checked = false;
+            filePhoto.Attributes.Clear();
+
+            errButton.InnerHtml = "**Clear Successfully!";
+        }
+    }
+}
