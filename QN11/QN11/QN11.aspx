b68d280b9f0516404ac88664f1fad0079a10cf93
mahendramahara <mahendramahara369@gmail.com>
2023-08-24 07:00:00 +0545

    Added solved ASP.NET project 🔥

---
+++ QN11/QN11/QN11.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="QN11.aspx.cs" Inherits="QN11.QN11" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Q.N 11</title>
+    <link href="QN11.css" rel="stylesheet" />
+</head>
+<body>
+    <section class="container">
+        <h1>Information Reading</h1>
+        <div class="errMessage-container">
+            <span id="errMessage" runat="server"></span>
+        </div>
+        <div class="message-container">
+            <h5>Click the 'Retrieve Data' button to view the information about the books.</h5>
+        </div>
+        <form id="form1" runat="server">
+            <div class="button-container">
+                <asp:Button Text="Retrieve Data" runat="server" ID="btnRetrieve" OnClick="btnRetrieve_Click" />
+            </div>
+            <div class="table-container">
+                <asp:GridView ID="GridView" runat="server"></asp:GridView>
+            </div>
+        </form>
+    </section>
+</body>
+</html>
---
+++ QN11/QN11/QN11.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace QN11
+{
+    public partial class QN11 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRetrieve_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection connect = new MySqlConnection("Server=localhost;Database=project;Uid=root;pwd=;");
+                connect.Open();
+
+                // SQL Query to retrieve all books
+                MySqlCommand command = new MySqlCommand("SELECT * FROM book_data", connect);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                // Display the data in GridView (you can use other controls based on your requirements)
+                if (dataTable.Rows.Count > 0)
+                {
+                    GridView.DataSource = dataTable;
+                    GridView.DataBind();
+                }
+                else
+                {
+                    errMessage.InnerText = "No data found.";
+                }
+            }
+            catch (Exception ex)
+            {
+                errMessage.InnerText = "Error retrieving books! **" + ex.Message;
+            }
+        }
+    }
+}
