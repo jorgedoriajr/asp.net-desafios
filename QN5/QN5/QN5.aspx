92e46508016bf7055eba844dcb3be9f7f4d42c8b
mahendramahara <mahendramahara369@gmail.com>
2023-11-12 07:00:00 +0545

    Added solved ASP.NET project 🔥

---
+++ QN5/QN5/QN5.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="QN5.aspx.cs" Inherits="QN5.QN5" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Q.N 5</title>
+    <link href="QN5.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container">
+        <div>
+            <h1>Informaton for DataBase</h1>
+        </div>
+        <form id="form1" runat="server">
+            <div>
+                <asp:Label Text="Name:" runat="server" ID="lblName" />
+                <asp:TextBox ID="txtName" runat="server" />
+                <span id="errName" runat="server"></span>
+            </div>
+            <div>
+                <asp:Label Text="Email:" runat="server" ID="lblEmail" />
+                <asp:TextBox ID="txtEmail" runat="server" TextMode="Email" />
+                <span id="errEmail" runat="server"></span>
+            </div>
+            <div>
+                <asp:Label Text="Password:" runat="server" ID="lblPassword" />
+                <asp:TextBox ID="txtPassword" runat="server" TextMode="Password" />
+                <span id="errPassword" runat="server"></span>
+            </div>
+            <div>
+                <asp:Label Text="Phone:" runat="server" ID="lblPhone" />
+                <asp:TextBox ID="txtPhone" runat="server" TextMode="Phone" />
+                <span id="errPhone" runat="server"></span>
+            </div>
+            <div>
+                <asp:Label Text="Gender:" runat="server" ID="lblGender" />
+                <div class="radioOption">
+                    <asp:RadioButton ID="rblMale" runat="server" Text="Male" Value="Male" GroupName="rblGender" />
+                    <asp:RadioButton ID="rblFemale" runat="server" Text="Female" Value="Female" GroupName="rblGender" />
+                    <asp:RadioButton ID="rblOther" runat="server" Text="Other" Value="Other" GroupName="rblGender" />
+                </div>
+                <span id="errGender" runat="server"></span>
+            </div>
+            <div>
+                <asp:Label Text="Faculty:" runat="server" ID="lblFaculty" />
+                <asp:DropDownList ID="ddlFaculty" runat="server">
+                    <asp:ListItem Text="Select Faculty" Value="" />
+                    <asp:ListItem Text="Humanities" Value="Humanities" />
+                    <asp:ListItem Text="Management" Value="Management" />
+                    <asp:ListItem Text="Science" Value="Science" />
+                </asp:DropDownList>
+                <span id="errFaculty" runat="server"></span>
+            </div>
+            <div class="btnCenter">
+                <asp:Button Text="Store Record" runat="server" ID="btnStoreRecord" OnClick="btnStoreRecord_Click" />
+                <span id="errStoreRecord" runat="server"></span>
+            </div>
+            <div>
+                <span id="errDataBase" runat="server" ></span>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ QN5/QN5/QN5.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Runtime.Remoting.Lifetime;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace QN5
+{
+    public partial class QN5 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Clear error messages on page load
+            ClearErrorMessages();
+        }
+
+        protected void btnStoreRecord_Click(object sender, EventArgs e)
+        {
+            // Clear error messages before validating
+            ClearErrorMessages();
+
+            // Retrieve user input
+            string name = txtName.Text;
+            string email = txtEmail.Text;
+            string password = txtPassword.Text;
+            string phone = txtPhone.Text;
+            string gender = GetSelectedGender();
+            string faculty = ddlFaculty.SelectedValue;
+
+            int error = 0;
+
+            // Validate user input
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                error++;
+                errName.InnerHtml = "**Enter your Name.";
+            }
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                error++;
+                errEmail.InnerHtml = "**Enter your email address.";
+            }
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                error++;
+                errPassword.InnerHtml = "**Enter a password.";
+            }
+            if (string.IsNullOrWhiteSpace(phone))
+            {
+                error++;
+                errPhone.InnerHtml = "**Enter your Phone Number.";
+            }
+            if (string.IsNullOrWhiteSpace(gender))
+            {
+                error++;
+                errGender.InnerHtml = "**Choose your Gender.";
+            }
+            if (string.IsNullOrWhiteSpace(faculty))
+            {
+                error++;
+                errFaculty.InnerHtml = "**Select your Faculty.";
+            }
+
+            if(error == 0)
+            {
+                try
+                {
+                    //Database Connectivety
+                    MySqlConnection connect = new MySqlConnection("Server=localhost;Database=project;Uid=root;pwd=;");
+                    connect.Open();
+
+                    //Sql Query To Record
+                    MySqlCommand command = new MySqlCommand("INSERT INTO registrations (Name, Email, Password, Phone, Gender, Faculty)\r\nVALUES (@name, @email, @password, @phone, @gender, @faculty)", connect);
+                    command.Parameters.AddWithValue("@name", name);
+                    command.Parameters.AddWithValue("@email", email);
+                    command.Parameters.AddWithValue("@password", password);
+                    command.Parameters.AddWithValue("@phone", phone);
+                    command.Parameters.AddWithValue("@gender", gender);
+                    command.Parameters.AddWithValue("@faculty", faculty);
+
+                    //execute query
+                    command.ExecuteNonQuery();
+                    errDataBase.InnerText = "Database Connection and Register Successfully";
+
+                }
+                catch (Exception ex)
+                {
+                    errDataBase.InnerText = "Error! **" + ex.Message;
+                }
+            }
+
+        }
+
+        private void ClearErrorMessages()
+        {
+            errName.InnerHtml = String.Empty;
+            errEmail.InnerHtml = String.Empty;
+            errPassword.InnerHtml = String.Empty;
+            errPhone.InnerHtml = String.Empty;
+            errGender.InnerHtml = String.Empty;
+            errFaculty.InnerHtml = String.Empty;
+        }
+
+        private string GetSelectedGender()
+        {
+            if (rblFemale.Checked)
+            {
+                return rblFemale.Text;
+            }
+            else if (rblMale.Checked)
+            {
+                return rblMale.Text;
+            }
+            else if (rblOther.Checked)
+            {
+                return rblOther.Text;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
