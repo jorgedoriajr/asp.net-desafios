90599ad857e53e28f37773f9782a23a81a6083fc
mahendramahara <mahendramahara369@gmail.com>
2023-12-31 07:00:00 +0545

    Added solved ASP.NET project 🔥

---
+++ QN13/QN13/QN13.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="QN13.aspx.cs" Inherits="QN13.QN13" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Q.N 13</title>
+    <link href="QN13.css" rel="stylesheet" />
+</head>
+<body>
+    <section class="container">
+        <h1>Information Delete</h1>
+        <div class="errMessage-container">
+            <span id="errMessage" runat="server"></span>
+        </div>
+        <form id="form1" runat="server">
+            <div class="read">
+                <div class="message-container">
+                    <h5>Click the 'Retrieve Data' button to view the information about the books.</h5>
+                </div>
+                <div class="button-container">
+                    <asp:Button Text="Retrieve Data" runat="server" ID="btnRetrieve" OnClick="btnRetrieve_Click" />
+                </div>
+                <div class="message-container">
+                    <h5>Here display the data before it was deleted:</h5>
+                </div>
+                <div class="table-container">
+                    <asp:GridView ID="GridViewBeforeDelete" runat="server"></asp:GridView>
+                </div>
+            </div>
+            <div class="delete">
+                <div class="message-container">
+                    <h5>Enter the old book ID you wish to edit.</h5>
+                </div>
+                <div class="deletableID-container">
+                    <asp:Label Text="Enter Book ID:" runat="server" ID="lblBookID" />
+                    <asp:TextBox runat="server" ID="txtBookID" />
+                    <span id="errBookID" runat="server"></span>
+                </div>
+                <div class="message-container">
+                    <h5>Click the 'Delete Record' button to update the information about the book.</h5>
+                </div>
+                <div class="button-container">
+                    <asp:Button Text="Delete Record" runat="server" ID="btnDelete" OnClick="btnDelete_Click" />
+                </div>
+                <div class="message-container">
+                    <h5>Here display the data after deleted:</h5>
+                </div>
+                <div class="table-container">
+                    <asp:GridView ID="GridViewAfterDelete" runat="server"></asp:GridView>
+                </div>
+            </div>
+        </form>
+    </section>
+</body>
+</html>
---
+++ QN13/QN13/QN13.aspx.cs
@@ -0,0 +1,119 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace QN13
+{
+    public partial class QN13 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRetrieve_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection connect = new MySqlConnection("Server=localhost;Database=project;Uid=root;pwd=;");
+                connect.Open();
+
+                // SQL Query to retrieve all books
+                MySqlCommand command = new MySqlCommand("SELECT * FROM book_data", connect);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                // Display the data in GridView (you can use other controls based on your requirements)
+                if (dataTable.Rows.Count > 0)
+                {
+                    GridViewBeforeDelete.DataSource = dataTable;
+                    GridViewBeforeDelete.DataBind();
+                }
+                else
+                {
+                    errMessage.InnerText = "No data found.";
+                }
+            }
+            catch (Exception ex)
+            {
+                errMessage.InnerText = "Error retrieving books! **" + ex.Message;
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            string selectedID = txtBookID.Text;
+
+            int error = 0;
+
+            if (string.IsNullOrWhiteSpace(selectedID))
+            {
+                error++;
+                errBookID.InnerHtml = "**Enter the book ID first.";
+            }
+            if (error == 0)
+            {
+                try
+                {
+                    // Database Connectivity
+                    MySqlConnection connect = new MySqlConnection("Server=localhost;Database=project;Uid=root;pwd=;");
+                    connect.Open();
+
+                    // SQL Query to remove record
+                    MySqlCommand command = new MySqlCommand("DELETE FROM book_data WHERE Book_ID = @bookid", connect);
+                    command.Parameters.AddWithValue("@bookid", selectedID);
+
+                    // Execute query
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        errMessage.InnerText = "Book removed successfully.";
+
+                        try
+                        {
+                            MySqlConnection connect1 = new MySqlConnection("Server=localhost;Database=project;Uid=root;pwd=;");
+                            connect1.Open();
+
+                            // SQL Query to retrieve all books
+                            MySqlCommand command1 = new MySqlCommand("SELECT * FROM book_data", connect1);
+                            MySqlDataAdapter adapter1 = new MySqlDataAdapter(command1);
+                            DataTable dataTable1 = new DataTable();
+                            adapter1.Fill(dataTable1);
+
+                            // Display the data in GridView (you can use other controls based on your requirements)
+                            if (dataTable1.Rows.Count > 0)
+                            {
+                                GridViewAfterDelete.DataSource = dataTable1;
+                                GridViewAfterDelete.DataBind();
+                            }
+                            else
+                            {
+                                errMessage.InnerText = "Error!";
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            errMessage.InnerText = "Error retrieving books! **" + ex.Message;
+                        }
+                    }
+                    else
+                    {
+                        errMessage.InnerText = "Book with the specified ID not found.";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    errMessage.InnerText = "Error! **" + ex.Message;
+                }
+            }
+        }
+    }
+}
